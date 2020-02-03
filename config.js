12d2f015b99df7510b8918b1272296f334e858ae
kaito8605 <60184970+kaito8605@users.noreply.github.com>
2020-02-03 13:08:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,67 +1,64 @@
 /**
- * DDDDDDD    EEEEEEE  RRRRRRR      OOOO     KK  KK    UU        UU      VV            VV      0000              44
- * DD    DD   EE       RR    RR   OO    OO   KK KK     UU        UU       VV          VV     00   000           44
- * DD     DD  EE       RR    RR  OO      OO  KKKK      UU        UU        VV        VV     00   00 00         44     
- * DD     DD  EEEEEEE  RRRRRRRR  OO      OO  KKKK      UU        UU         VV      VV      00  00  00        44  44
- * DD     DD  EEEEEEE  RRRR      OO      OO  KK KK     UU        UU          VV    VV       00 00   00       44   44
- * DD     DD  EE       RR RR     OO      OO  KK  KK    UU        UU           VV  VV        0000    00      444444444444 
- * DD    DD   EE       RR  RR     OO    OO   KK   KK    UU      UU             VVVV          00    00  ###        44
- * DDDDDDD    EEEEEEE  RR   RR      OOOO     KK    KK     UUUUUU                VV             0000    ###        44
+██████╗ ███████╗██████╗  ██████╗ ██╗  ██╗██╗   ██╗    ██╗   ██╗ ██████╗    ███████╗
+██╔══██╗██╔════╝██╔══██╗██╔═══██╗██║ ██╔╝██║   ██║    ██║   ██║██╔═████╗   ██╔════╝
+██║  ██║█████╗  ██████╔╝██║   ██║█████╔╝ ██║   ██║    ██║   ██║██║██╔██║   ███████╗
+██║  ██║██╔══╝  ██╔══██╗██║   ██║██╔═██╗ ██║   ██║    ╚██╗ ██╔╝████╔╝██║   ╚════██║
+██████╔╝███████╗██║  ██║╚██████╔╝██║  ██╗╚██████╔╝     ╚████╔╝ ╚██████╔╝██╗███████║
+╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝       ╚═══╝   ╚═════╝ ╚═╝╚══════╝
  */
 
  /**
-  ___ _   _ ____ _____ ____  _   _  ____ _____ ___ ___  _   _ ____  
- |_ _| \ | / ___|_   _|  _ \| | | |/ ___|_   _|_ _/ _ \| \ | / ___| 
-  | ||  \| \___ \ | | | |_) | | | | |     | |  | | | | |  \| \___ \ 
-  | || |\  |___) || | |  _ <| |_| | |___  | |  | | |_| | |\  |___) |
- |___|_| \_|____/ |_| |_| \_\\___/ \____| |_| |___\___/|_| \_|____/ 
+██╗███╗   ██╗███████╗████████╗██████╗ ██╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║   ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
+██║██╔██╗ ██║███████╗   ██║   ██████╔╝██║   ██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
+██║██║╚██╗██║╚════██║   ██║   ██╔══██╗██║   ██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
+██║██║ ╚████║███████║   ██║   ██║  ██║╚██████╔╝╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
 
- 1. Remplire les champs ci-dessous en laissant les guillemets
+ 1. Remplir les champs ci-dessous en laissant les guillemets
  2. ouvrir cmd dans le dossier et faire node index.js
  3. Après que le script ait installé les dépendances, relancer le index avec node index
  4. Profitez du selfbot!!
 
- Note: les dépendances s'installent tout seul au premier démmarage.
+ Note: les dépendances s'installent toutes seules au premier démmarage.
 
 */
 
 /**
-   ____ ___  _   _ _____ ___ ____ _   _ ____      _  _____ ___ ___  _   _ 
-  / ___/ _ \| \ | |  ___|_ _/ ___| | | |  _ \    / \|_   _|_ _/ _ \| \ | |
- | |  | | | |  \| | |_   | | |  _| | | | |_) |  / _ \ | |  | | | | |  \| |
- | |__| |_| | |\  |  _|  | | |_| | |_| |  _ <  / ___ \| |  | | |_| | |\  |
-  \____\___/|_| \_|_|   |___\____|\___/|_| \_\/_/   \_\_| |___\___/|_| \_|
-                                                                          
+ ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗ ██╗   ██╗██████╗  █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝ ██║   ██║██╔══██╗██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗██║   ██║██████╔╝███████║   ██║   ██║██║   ██║██╔██╗ ██║
+██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║██║   ██║██╔══██╗██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝╚██████╔╝██║  ██║██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+
  */
-const token = "{user-token}"//ton token
-const prefix = "{user-prefix}"//ton prefix
-const color = "{user-color}"//"00ffb2"//la couleur des embeds en hexadécimal ou une couleur supporté par discord.js
-const premium = "{user-premium-key}"//ton code premium si tu en as un. notez que les clés ne sont pas partageable et résulteront à l'effacement de ces dernières.
-const twitch = "{user-twitch}"//ton url twitch [𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙍𝙀𝙌𝙐𝙄𝙎]
-const team = "{user-team}" // Si vous faites parti d'une team de raid, vous pouvez peut être avoir des commandes premium et des commandes personalisé à la team.
-const nsfw = false // Bypass la verif du salon nsfw(ne vérifie pas si le salon actuel est nsfw pour les commandes nsfw.) false = ne bypass pas, true = bypass
-//Entrez le code de team fourni par le leader ou un staff.
+
+const token = "NjM2NjIxOTAxOTIxMzg2NTI3.XjfRsA.8IDkGmm4jyJX346RyfPI06xV7cE"//ton token
+const prefix = "$"//ton prefix
+const premium = "" //ton code premium si tu en as un. notez que les clés ne sont pas partageable et résulteront à l'effacement de ces dernières.
 
 /**
-  _____ _    ___  
- |  ___/ \  / _ \ 
- | |_ / _ \| | | |
- |  _/ ___ \ |_| |
- |_|/_/   \_\__\_\
- 
+███████╗ █████╗  ██████╗ 
+██╔════╝██╔══██╗██╔═══██╗
+█████╗  ███████║██║   ██║
+██╔══╝  ██╔══██║██║▄▄ ██║
+██║     ██║  ██║╚██████╔╝
+╚═╝     ╚═╝  ╚═╝ ╚══▀▀═╝ 
+
  Q: Pourquoi avoir obfusquer le code?
  R: Car nous ne voulons pas que le code soit publique et que tout le monde puisse l'intégrer à son bot ou son selfbot. 
  Aussi car les commandes premium sont inclusent mais se débloquent uniquement si le code est valable. 
  Sinon, il serait hyper facile de s'attribuer le premium sans code valide.
  
  Q: Est-ce que je peux partager mon code premium?
- R: Oui et non. Si vous l'utilisez sur 2 comptes maximum sur le même ordinateur, oui mais si c'est pour donner à tout discord, c'est non et votre clé sera supprimé.
+ R: Oui et non. tant que c'est vos comptes, oui mais si c'est pour donner à tout discord, c'est non et votre clé sera supprimé.
 
  Q: Si j'ai le premium de la première version, est-ce que je le garde quand il y a une mise à jour du bot?
  R: Oui, bien sûr. Nous estimons que ceci est une nécessité et que nous ne devrions pas payer plusieurs fois.
 
  Q: J'ai acheté un code chez quelqu'un d'autre pour moins cher. Je me suis fais arnaquer, que puis-je faire?
- R: Nous ne pouvons rien pour vous. Il faut acheter son code chez ๖̶̶̶ζJeanOUINAζ͜͡ 𝙈𝙉𝑳͜͡ζ𝐎𝐬𝐦𝐨𝐙ȺƐƓ#6666 UNIQUEMENT.
+ R: Nous ne pouvons rien pour vous. Il faut acheter son code chez jeanouina (allez me mp depuis le serveur deroku) UNIQUEMENT.
 
  Q: Comment puis-je faire en sorte que le selfbot soit allumé 24h/24h 7j/7j ?
  R: Nous proposons des solutions d'hebergement. Elle sont de 3€/mois. 
@@ -80,12 +77,9 @@ const nsfw = false // Bypass la verif du salon nsfw(ne vérifie pas si le salon 
 
 
  //Les 7 lignes ci-dessous sont pour l'utilisation de vos informations fournies plus haut dans le selfbot. veuillez ne pas modifier ces prochaines lignes.
-module.exports = {
-    token,
-    prefix,
-    color,//NE PAS TOUCHER
-    premium,
-    twitch,
-    team,
-    nsfw
-}
+
+module.exports = {//NE PAS TOUCHER
+    token,        //NE PAS TOUCHER
+    prefix,       //NE PAS TOUCHER
+    premium       //NE PAS TOUCHER
+}                 //NE PAS TOUCHER
